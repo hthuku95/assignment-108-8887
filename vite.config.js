0f2782fd76075a1a2207f4c9f4a9d7fa05d2bea5
Harry thuku <38860432+hthuku95@users.noreply.github.com>
2025-08-16 16:19:59 +0300

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "~bootstrap/scss/bootstrap";`
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['vue', 'vue-router', 'axios', 'bootstrap']
+  }
+})
